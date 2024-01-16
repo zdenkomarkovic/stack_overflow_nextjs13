d08e856fa0cb6bb1c149e4432e1f3b59d30b5ba1
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-01-16 20:28:59 +0100

    fix2

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,11 +1,8 @@
-import Link from 'next/link';
-import React from 'react'
-import RenderTag from '../shared/RenderTag';
-import Metric from '../shared/Metric';
-//import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
-import { SignedIn } from '@clerk/nextjs';
-import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
-//import EditDeleteAction from '../shared/EditDeleteAction';
+import Link from "next/link";
+import React from "react";
+import RenderTag from "../shared/RenderTag";
+import Metric from "../shared/Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 
 interface QuestionProps {
   _id: string;
@@ -18,17 +15,14 @@ interface QuestionProps {
     _id: string;
     name: string;
     picture: string;
-    //clerkId: string;
   };
-  upvotes: string[];
+  upvotes: number[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
-  //clerkId?: string | null;
 }
 
 const QuestionCard = ({
-  //clerkId,
   _id,
   title,
   tags,
@@ -36,25 +30,23 @@ const QuestionCard = ({
   upvotes,
   views,
   answers,
-  createdAt
+  createdAt,
 }: QuestionProps) => {
-  //const showActionButtons = clerkId && clerkId === author.clerkId;
-
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
-          {getTimestamp(createdAt)}
+            {getTimestamp(createdAt)}
           </span>
           <Link href={`/question/${_id}`}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
-              {title} 
+              {title}
             </h3>
           </Link>
         </div>
       </div>
-      
+
       <div className="mt-3.5 flex flex-wrap gap-2">
         {tags.map((tag) => (
           <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
@@ -62,42 +54,41 @@ const QuestionCard = ({
       </div>
 
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
-          <Metric 
-              imgUrl="/assets/icons/avatar.svg"
-              alt="user"
-              value={author.name}
-              title={` - asked ${getTimestamp(createdAt)}`}
-              href={`/profile/${author._id}`}
-              isAuthor
-              textStyles="body-medium text-dark400_light700"
+        <Metric
+          imgUrl="/assets/icons/avatar.svg"
+          alt="user"
+          value={author.name}
+          title={` - asked ${getTimestamp(createdAt)}`}
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="Upvotes"
+          value={formatAndDivideNumber(upvotes)}
+          title=" Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
+          <Metric
+            imgUrl="/assets/icons/message.svg"
+            alt="message"
+            value={formatAndDivideNumber(answers.length)}
+            title="Answers"
+            textStyles="small-medium text-dark400_light800"
           />
-          <Metric 
-              imgUrl="/assets/icons/like.svg"
-              alt="Upvotes"
-              value={formatAndDivideNumber(upvotes)}
-              title=" Votes"
-              textStyles="small-medium text-dark400_light800"
+          <Metric
+            imgUrl="/assets/icons/eye.svg"
+            alt="eye"
+            value={formatAndDivideNumber(views)}
+            title=" Views"
+            textStyles="small-medium text-dark400_light800"
           />
-          <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
-            <Metric 
-              imgUrl="/assets/icons/message.svg"
-              alt="message"
-              value={formatAndDivideNumber(answers.length)}
-              title="Answers"
-              textStyles="small-medium text-dark400_light800"
-            />
-            <Metric 
-              imgUrl="/assets/icons/eye.svg"
-              alt="eye"
-              value={formatAndDivideNumber(views)}
-              title=" Views"
-              textStyles="small-medium text-dark400_light800"
-            />
-          </div>
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default QuestionCard
+export default QuestionCard;
