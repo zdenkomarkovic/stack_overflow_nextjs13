19ad36d5f8de4f505a378be1ef185d02b259736f
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-01-18 02:06:43 +0100

    something

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,50 +1,81 @@
-"use server"
+"use server";
 
-import Question from "@/database/question.model"
-import { connectToDatabase } from "../mongoose"
-import Tag from "@/database/tag.model"
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types"
-import User from "@/database/user.model"
-import { revalidatePath } from "next/cache"
+import Question from "@/database/question.model";
+import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
+import {
+  CreateQuestionParams,
+  GetQuestionByIdParams,
+  GetQuestionsParams,
+} from "./shared.types";
+import User from "@/database/user.model";
+import { revalidatePath } from "next/cache";
 
-export async function getQuestions(params:GetQuestionsParams) {
- try {
-  connectToDatabase()
+export async function getQuestions(params: GetQuestionsParams) {
+  try {
+    connectToDatabase();
 
-  const questions = (await Question.find({}).populate({path: 'tags', model: Tag}).populate({path: 'author', model: User}).sort({createdAt: -1}))
-return {questions}
- } catch (error) {
-  console.log(error);
-  throw error
-  
-  
- } 
+    const questions = await Question.find({})
+      .populate({ path: "tags", model: Tag })
+      .populate({ path: "author", model: User })
+      .sort({ createdAt: -1 });
+    return { questions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
 export async function createQuestion(params: CreateQuestionParams) {
-   try {
-    connectToDatabase()
-    const {title, content, tags, author, path} = params
+  try {
+    connectToDatabase();
+    const { title, content, tags, author, path } = params;
 
     const question = await Question.create({
-      title, content, author
-    })
+      title,
+      content,
+      author,
+    });
 
-    const tagDocuments = []
+    const tagDocuments = [];
 
     for (const tag of tags) {
       const existingTag = await Tag.findOneAndUpdate(
-        { name: { $regex: new RegExp(`^${tag}$`, "i") } }, 
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
         { $setOnInsert: { name: tag }, $push: { questions: question._id } },
         { upsert: true, new: true }
-      )
-      tagDocuments.push(existingTag._id)
+      );
+      tagDocuments.push(existingTag._id);
     }
     await Question.findByIdAndUpdate(question._id, {
-      $push: {tags: {$each:tagDocuments }}
-    })
-revalidatePath(path)
-   } catch (error) {
-    
-   } 
+      $push: { tags: { $each: tagDocuments } },
+    });
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
+
+export const getQuestionById = async (params: GetQuestionByIdParams) => {
+  try {
+    connectToDatabase();
+    const { questionId } = params;
+
+    const question = await Question.findById(questionId)
+      .populate({
+        path: "tags",
+        model: Tag,
+        select: "_id name",
+      })
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id clerkId name picture",
+      });
+    return question;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
---
+++ lib/actions/tag.action.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import Tag from "@/database/tag.model";
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    connectToDatabase();
+    const { userId } = params;
+    const user = await User.findById(userId);
+    if (!user) throw new Error("User not found");
+    return [
+      { _id: "1", name: "tag" },
+      { _id: "2", name: "tag2" },
+    ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export const getAllTags = async (params: GetAllTagsParams) => {
+  try {
+    connectToDatabase();
+    const tags = await Tag.find({});
+    return { tags };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
